e81bf0adb994045a89fba01e032ce6aac7e41158
feed0 <dogmas-gutters.07@icloud.com>
2025-10-22 12:10:38 -0300

    [WF]

--- WeatherForecast/ContentView.swift
+++ WeatherForecast/ContentView.swift
@@ -9,16 +9,60 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack {
+            DayForecast(day: "Mon", isRainy: false, high: 70, low: 50)
+            
+            DayForecast(day: "Tue", isRainy: true, high: 60, low: 40)
+            
+            DayForecast(day: "Wed", isRainy: true, high: 50, low: 10)
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct DayForecast: View {
+    let day: String
+    let isRainy: Bool
+    let high: Int
+    let low: Int
+    
+    var iconName: String {
+        if isRainy {
+            "cloud.rain.fill"
+        } else {
+            "sun.max.fill"
+        }
+    }
+    
+    var iconColor: Color {
+        if isRainy {
+            Color.blue
+        } else {
+            Color.yellow
+        }
+    }
+        
+    var body: some View {
+        VStack {
+            Text(day)
+                .font(Font.largeTitle)
+            
+            Image(systemName: iconName)
+                .foregroundStyle(iconColor)
+                .padding(5)
+            
+            VStack(alignment: .trailing) {
+                Text("High: \(high)º")
+                    .fontWeight(Font.Weight.semibold)
+                Text("Low: \(low)º")
+                    .fontWeight(Font.Weight.medium)
+                    .foregroundStyle(Color.secondary)
+            }
+            
+        }
+        .padding()
+    }
 }
