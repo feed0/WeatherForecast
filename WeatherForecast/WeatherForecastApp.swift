e8e9ea8f2ca5fcf63fcebb14111e1c931d0d93fc
feed0 <dogmas-gutters.07@icloud.com>
2025-10-22 10:53:46 -0300

    Initial Commit

---
+++ WeatherForecast/WeatherForecastApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherForecastApp.swift
+//  WeatherForecast
+//
+//  Created by Felipe Eduardo Campelo Ferreira Osorio on 22/10/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherForecastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
